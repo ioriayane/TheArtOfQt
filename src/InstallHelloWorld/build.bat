086687ff08151d4dbe5f62d38c047da5daef01f4
Takayuki ORITO <iori.ayane@gmail.com>
2014-10-13 22:36:02 +0900

    add online installer setting

--- src/InstallHelloWorld/build.bat
+++ src/InstallHelloWorld/build.bat
@@ -1,17 +1,44 @@
+@echo off
+
+REM ------------------------------------------
+REM オンラインかオフラインかを決定
+
+
+if '%2'=='offline' goto OFFLINE
+goto ONLINE
+
+:OFFLINE
+echo offline
+set OFFLINE=--offline-only
+goto ARCH
+
+:ONLINE
+echo online
+set OFFLINE=
+goto ARCH
+
+
+
+REM ------------------------------------------
+REM アーキテクチャを決定
+:ARCH
 
 if '%1'=='x86' goto X86
 if '%1'=='x64' goto X64
 
 
-echo "build.bat x86|x64"
+echo "build.bat x86|x64 [offline]"
 
 goto QUIT
 
 
 REM ------------------------------------------
+REM アーキテクチャごとの処理
+
+REM ------------------------------------------
 :X86
 echo x86
-C:\Qt\QtIFW-1.5-32\bin\binarycreator.exe -c config\config.xml -p packages HelloSetup.exe
+C:\Qt\QtIFW-1.5-32\bin\binarycreator.exe %OFFLINE% -c config\config.xml -p packages HelloSetup.exe
 
 goto QUIT
 
@@ -19,12 +46,12 @@ goto QUIT
 REM ------------------------------------------
 :X64
 echo x64
-C:\Qt\QtIFW-1.5-64\bin\binarycreator.exe -c config\config.xml -p packages HelloSetup.exe
+C:\Qt\QtIFW-1.5-64\bin\binarycreator.exe %OFFLINE% -c config\config.xml -p packages HelloSetup.exe
 
 goto QUIT
 
 
 REM ------------------------------------------
-
+REM 終了
 :QUIT
 
