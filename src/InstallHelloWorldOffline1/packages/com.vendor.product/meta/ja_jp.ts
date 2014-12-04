7ea7d916e617f636afeb9688a79133519a385635
Takayuki ORITO <iori.ayane@gmail.com>
2014-12-04 23:48:50 +0900

    installer:update ts file

--- src/InstallHelloWorldOffline1/packages/com.vendor.product/meta/ja_jp.ts
+++ src/InstallHelloWorldOffline1/packages/com.vendor.product/meta/ja_jp.ts
@@ -2,16 +2,10 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
-    <name>FinishAndOpenForm</name>
+    <name>dummy</name>
     <message>
-        <location filename="finishandopen.ui" line="17"/>
-        <source>Open Readme.txt</source>
-        <translation>Readme.txtを開く</translation>
-    </message>
-    <message>
-        <location filename="finishandopen.ui" line="24"/>
-        <source>Run Application</source>
-        <translation type="unfinished">アプリケーションを実行する</translation>
+        <source>dummy</source>
+        <translation>dummy</translation>
     </message>
 </context>
 </TS>
